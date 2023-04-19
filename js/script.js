c7a6ea023dfae150e9c09b52aa2581b2ff2c4f3b
Sota AKASAKA <s214229y@st.go.tuat.ac.jp>
2023-04-19 14:38:37 +0900

    add humburger menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function showMessage() {
     alert('Hello World!');
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector('.menu-items');
+
+menuBtn.addEventListener('click', () => {
+  menuItems.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+});
